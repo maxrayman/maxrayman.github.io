00d2129be5941beb06bf3fcb2cd69dc69ac89f31
maxrayman <maxrayman@users.noreply.github.com>
2025-09-15 15:22:54 +0000

    deploy: maxrayman/hugo-blowfish-portfolio@dceef1e8a34afeb433898c3c7d9f7b6a49f07c74

---
+++ blog/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1757949769',resources: [
+      '/blog/assets/css/jekyll-theme-chirpy.css',
+      '/blog/',
+      
+        '/blog/categories/',
+      
+        '/blog/tags/',
+      
+        '/blog/archives/',
+      
+        '/blog/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://maxrayman.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
